6d62a7a4b2266b5c104c2873c7c811751974694c
danilo.volles.cofen <danilo.volles@outlook.com>
2023-08-03 16:46:08 -0300

    Adicionar, buscar por id e deletar objetos

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import express from 'express'
 
 const app = express()
 
+// Indica para o express ler body com o json   
+app.use(express.json())
+
 // Mock
 const CARROS = [
     {id: 1, carro: 'Corolla', marca: 'Toyota'},
@@ -10,6 +13,17 @@ const CARROS = [
     {id: 4, carro: 'Argo', marca: 'Fiat'}
 ]
 
+// Esse retorno é um objeto
+// Retorna o objeto por id
+function buscarCarroPorId(id) {
+    return CARROS.filter( carro => carro.id == id)
+}
+
+// Pegar a posição, o index, do elemento no array
+function buscarIndexCarro(id) {
+    return CARROS.findIndex ( carro => carro.id == id)
+}
+
 // Criar rota padrão ou raiz
 app.get('/', (req, res) => {
     res.send('Hello, World!')
@@ -17,6 +31,22 @@ app.get('/', (req, res) => {
 
 app.get('/carros', (req,res) => {
     res.status(200).send(CARROS)
+})
+
+app.get('/carros/:id', (req, res) => {
+    // let index = req.params.id // Criar uma variável index, que recebe como parametro um ID
+    res.json(buscarCarroPorId(req.params.id))
+})
+
+app.post('/carros', (req, res) => {
+    CARROS.push(req.body)
+    res.status(201).send('Carro adicionado com sucesso!')
+})
+
+app.delete('/carros/:id', (req, res) => {
+    let index = buscarIndexCarro(req.params.id)
+    CARROS.splice(index, 1)
+    res.send(`Objeto ${req.params.id} excluído com sucesso`)
 })
 
 export default app
