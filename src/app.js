0b01cc009ce04694d93eee0213b4304889c0d021
danilo.volles.cofen <danilo.volles@outlook.com>
2023-08-04 11:36:04 -0300

    rota put. refatoração de variável.

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.json())
 
 // Mock
-const CARROS = [
+const carros = [
     {id: 1, carro: 'Corolla', marca: 'Toyota'},
     {id: 2, carro: 'Civic', marca: 'Honda'},
     {id: 3, carro: 'HB20', marca: 'Hyundai'},
@@ -16,12 +16,12 @@ const CARROS = [
 // Esse retorno é um objeto
 // Retorna o objeto por id
 function buscarCarroPorId(id) {
-    return CARROS.filter( carro => carro.id == id)
+    return carros.filter( carro => carro.id == id)
 }
 
 // Pegar a posição, o index, do elemento no array
 function buscarIndexCarro(id) {
-    return CARROS.findIndex ( carro => carro.id == id)
+    return carros.findIndex ( carro => carro.id == id)
 }
 
 // Criar rota padrão ou raiz
@@ -30,7 +30,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/carros', (req,res) => {
-    res.status(200).send(CARROS)
+    res.status(200).send(carros)
 })
 
 app.get('/carros/:id', (req, res) => {
@@ -39,15 +39,21 @@ app.get('/carros/:id', (req, res) => {
 })
 
 app.post('/carros', (req, res) => {
-    CARROS.push(req.body)
+    carros.push(req.body)
     res.status(201).send('Carro adicionado com sucesso!')
 })
 
 app.delete('/carros/:id', (req, res) => {
     let index = buscarIndexCarro(req.params.id)
-    CARROS.splice(index, 1)
+    carros.splice(index, 1)
     res.send(`Objeto ${req.params.id} excluído com sucesso`)
 })
 
-export default app
+app.put('/carros/:id', (req, res) => {
+    let index = buscarIndexCarro(req.params.id)
+    carros[index].carro = req.body.carro
+    carros[index].marca = req.body.marca
+    res.json(carros)
+})
 
+export default app
