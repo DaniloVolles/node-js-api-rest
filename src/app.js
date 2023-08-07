b4103909ccbbe22c8aba4ce648fa6d38a880604d
danilo.volles.cofen <danilo.volles@outlook.com>
2023-08-07 13:30:01 -0300

    Correção de lógica

--- src/app.js
+++ src/app.js
@@ -3,10 +3,10 @@ import routes from './routes.js'
 
 const app = express()
 
-// usar o routes
-app.use(routes)
-
 // Indica para o express ler body com o json   
 app.use(express.json())
+
+// usar o routes
+app.use(routes)
 
 export default app
