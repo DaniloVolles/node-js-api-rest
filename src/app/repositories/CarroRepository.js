a936303d875678fba6ff653c0eb1f07fc7f692f8
danilo.volles.cofen <danilo.volles@outlook.com>
2023-08-07 13:01:25 -0300

    Refatoração método Promisse

--- src/app/repositories/CarroRepository.js
+++ src/app/repositories/CarroRepository.js
@@ -1,81 +1,31 @@
-import conexao from "../database/conexao.js"
+import { consulta } from "../database/conexao.js"
 
 class CarroRepository{
     // CRUD
     create(carro){
         const sql = 'INSERT INTO carros SET ?;'
-        return new Promise((resolve, reject) => {
-            conexao.query(sql, carro, (erro, resultado) => {
-                if (erro) {
-                    return reject('Impossível cadastrar')
-                } else {
-                    const row = JSON.parse(JSON.stringify(resultado))
-                    return resolve(row)
-                }
-            })
-        })
+        return consulta(sql, carro, 'Não foi possível cadastrar.')
     }
-
 
     findAll(res){
         const sql = 'SELECT * FROM carros;'
-        return new Promise((resolve, reject) => {
-            conexao.query(sql, (erro, resultado) => {
-                if (erro) {
-                    return reject('Impossível localizar')
-                } else {
-                    const row = JSON.parse(JSON.stringify(resultado))
-                    return resolve(row)
-                }
-            })
-        })
+        return consulta(sql, 'Não foi possível localizar.')
     }
-
 
     findById(id){
         const sql = 'SELECT * FROM carros WHERE id=?;'
-        return new Promise((resolve, reject) => {
-            conexao.query(sql, id, (erro, resultado) => {
-                if (erro) {
-                    return reject('Impossível localizar')
-                } else {
-                    const [ rows ] = JSON.parse(JSON.stringify(resultado))
-                    console.log(rows)
-                    return resolve(rows)
-                }
-            })
-        })
+        return consulta(sql, id, 'Não foi possível localizar.')
     }
-
 
     update(carro, id) {
         const sql = 'UPDATE carros SET ? WHERE id=?;'
-        return new Promise((resolve, reject) => {
-            conexao.query(sql, [carro, id], (erro, resultado) => {
-                if (erro) {
-                    return reject('Impossível localizar')
-                } else {
-                    const rows = JSON.parse(JSON.stringify(resultado))
-                console.log(rows)
-                return resolve(rows)
-                }
-            })
-        })
+        return consulta(sql, [carro, id], 'Não foi possível atualizar.')
     }
-
 
     delete(id){
         const sql = 'DELETE FROM carros WHERE id=?;'
-        return new Promise((resolve, reject) => {
-            conexao.query(sql, id, (erro, resultado) => {
-                if (erro) return reject('Impossível apagar')
-                const rows = JSON.parse(JSON.stringify(resultado))
-                console.log(rows)
-                return resolve(rows)
-            })
-        })
+        return consulta(sql, id, 'Não foi possível apagar.')
     }
-
 
 }
 
